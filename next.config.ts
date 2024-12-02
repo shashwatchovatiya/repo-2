a4e86ec3893f6093a4e33bb9bf2569dfa954e128
Shashwat Chovatiya <132459393+shashwatchovatiya@users.noreply.github.com>
2024-12-02 12:19:14 +0530

    first component create

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import path from "path";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@repo-1": path.resolve(__dirname, "../repo-1/src"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
